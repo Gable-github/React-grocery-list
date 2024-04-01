912b9deaf58779cadeb6869a52feabce4a722dfa
Gabryel <gabryelsoh@gmail.com>
2024-04-01 10:15:51 +0800

    form.tsx created

---
+++ React-grocery-list/src/components/Form.tsx
@@ -0,0 +1,73 @@
+import { Field, FieldValue, FieldValues, useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
+
+const schema = z.object({
+  name: z.string().min(3, { message: "Require at least 3 characters" }),
+  amount: z
+    .number({ invalid_type_error: "amount needs to be specified" })
+    .min(1, { message: "require at least 1 item" }),
+  category: z.string(),
+});
+
+type FormData = z.infer<typeof schema>;
+
+const Form = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>({ resolver: zodResolver(schema) });
+
+  const onSubmit = (data: FieldValues) => console.log(data);
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div className="mb-3">
+        <label htmlFor="name" className="form-label">
+          Name
+        </label>
+        <input
+          {...register("name")}
+          id="name"
+          type="text"
+          className="form-control"
+        />
+        {errors.name && <p className="text-danger">{errors.name.message}</p>}
+      </div>
+      <div className="mb-3">
+        <label htmlFor="amount" className="form-label">
+          Amount
+        </label>
+        <input
+          {...register("amount", { valueAsNumber: true })}
+          id="amount"
+          type="number"
+          className="form-control"
+        />
+        {errors.amount && (
+          <p className="text-danger">{errors.amount.message}</p>
+        )}
+      </div>
+      <div className="mb-3">
+        <label htmlFor="category" className="form-label">
+          Category
+        </label>
+        <select
+          {...register("category")}
+          id="category"
+          className="form-control"
+        >
+          <option>Meats</option>
+          <option>Vegtables</option>
+          <option>Fruits</option>
+        </select>
+      </div>
+      <button className="btn btn-primary" type="submit">
+        Add to list
+      </button>
+    </form>
+  );
+};
+
+export default Form;
